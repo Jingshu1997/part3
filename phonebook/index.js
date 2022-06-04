913346a1c65e589dbcc7f2f1761c83374b350d65
ZHANG_Jingshu <zhangjingshu1010@gmail.com>
2022-06-04 22:12:32 +0200

    test2

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+
+const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -8,11 +10,18 @@ const Person = require('./models/person')
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
+
 morgan.token('type', (req) => {
   return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status  :res[content-length] - :response-time ms :type '))
+
+const PORT=process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
@@ -29,25 +38,12 @@ app.post('/api/persons', (request, response, next) => {
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
-
-app.get('/info', (request, response) => {
-  Person.countDocuments({}, (error, count) => {
-    if (error) {
-      response.send(error)
-    } else {
-      const phonebookInfo = `Phonebook has info for ${count} people </p> <p> ${new Date()}</p>`
-      response.send(phonebookInfo)
-    }
-  })
-})
-
 app.get('/api/persons', (request, response,next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
     .catch(error => next(error))
 })
-
 app.get('/api/persons/:id', (request, response,next) => {
 
   Person.findById(request.params.id)
@@ -61,11 +57,15 @@ app.get('/api/persons/:id', (request, response,next) => {
     .catch(error => next(error))
 })
 
+app.get('/info', (request, response) => {
+  Person.countDocuments({}, (error, count) => {
+      const phonebookInfo = `Phonebook has info for ${count} people </p> <p> ${new Date()}</p>`
+      response.send(phonebookInfo)
+  })
+})
+
 app.delete('/api/persons/:id', (request, response,next) => {
-
-
-
-
+  
   Person.findByIdAndRemove(request.params.id)
     .then(() => {
       response.status(204).end()
@@ -102,7 +102,3 @@ const errorHandler = (error, request, response, next) => {
 }
 app.use(errorHandler)
 
-const PORT=process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
